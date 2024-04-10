baff11a5d4b1e6f4b3fe7f3e6be8ed6565d360a7
Андрей Иванов <ivanov.andrevv@gmail.com>
2024-04-10 13:26:25 +0200

    Register model User in admin-panel.

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'last_name', 'first_name', 'email')
