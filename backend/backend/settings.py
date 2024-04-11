4db4a6178ca043b621abf51929fdcc38d211eb30
Андрей Иванов <ivanov.andrevv@gmail.com>
2024-04-11 11:07:55 +0200

    Correct typo.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
     'users',
     'api'
 ]
@@ -133,3 +133,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
