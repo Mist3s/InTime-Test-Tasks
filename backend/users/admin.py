0194683c558ff79f417e3a98835573888bded6fc
Mist3s <ivanov.andrevv@gmail.com>
2024-04-13 16:36:45 +0200

    Зарегистрировать модель кода активации в админке.

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, AuthCode
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'last_name', 'first_name', 'email')
+    list_display = (
+        'id',
+        'username',
+        'last_name',
+        'first_name',
+        'email'
+    )
+
+
+@admin.register(AuthCode)
+class AuthCodeAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'code',
+        'user',
+        'datetime_end',
+        'used'
+    )
