8d94abd3b4e0e8bb28551b7bdb762f060472dcd1
Андрей Иванов <ivanov.andrevv@gmail.com>
2024-04-16 14:25:31 +0200

    Add API documentation.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from django.views.generic import TemplateView
 
 from .views import UserViewSet
 
@@ -10,5 +11,13 @@ router_v1.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
+    path('v1/docs/', TemplateView.as_view(
+        template_name='docs/redoc.html',
+        content_type='text/html'
+    )),
+    path('v1/docs/openapi-schema.yml', TemplateView.as_view(
+        template_name='docs/openapi-schema.yml',
+        content_type='text/yaml'
+    )),
 
 ]
